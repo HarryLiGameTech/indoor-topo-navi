9e0fdc14c8fdd2919b05d17d7e5966771593fe2c
user <example@anonymous.com>
2025-11-27 01:37:55 +0800

    intermediate0137

--- toponavi-dsl/src/main/scala/surfacelang/TopoTerm.scala
+++ toponavi-dsl/src/main/scala/surfacelang/TopoTerm.scala
@@ -1,27 +1,37 @@
 package surfacelang
 
-import corelang.{Env, Type}
+import corelang.{Environment, Identifier, Type, Value, Term, Expr, Env, TypeOnlyEnvironment, TypeEnvironment}
 
 type Context = Env
 
 case class TopoRoot(
+  name: String,
   params: List[(String, Type)],
   submaps: Set[TopoMap],
   transportations: Set[TopoTransportation],
   context: Context,
 ) {
-
+  def evalaute(ctx: Context = context): Value = {
+    corelang.Interpreter.eval(toTerm)(using env)
+  }
 }
 
 case class TopoTransportation(
+  name: String,
   context: Context,
 )
 
 case class TopoMap(
-
+  name: String,
   context: Context,
 )
 
 case class Constraint(
+  name: String,
   context: Context,
-)
+) {
+  def check(ctx: Context = context): Boolean = {
+    // Placeholder for constraint checking logic
+    true
+  }
+}
