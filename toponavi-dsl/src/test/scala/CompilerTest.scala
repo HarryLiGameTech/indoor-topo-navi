89b684a10a691dca84c72fbeeefc4f71d212af71
user <example@anonymous.com>
2026-03-09 17:25:23 +0800

    chores(DSL): intermediate1725

--- toponavi-dsl/src/test/scala/CompilerTest.scala
+++ toponavi-dsl/src/test/scala/CompilerTest.scala
@@ -8,95 +8,109 @@ import pprint.pprintln
 
 class CompilerTest extends AnyFunSuite with should.Matchers {
 
-//  test("Compiler should parse and elaborate example project") {
-//    // 1. Create a temporary directory for the test project
-//    val tempDir = Files.createTempDirectory("toponavi_test_project").toFile
-//    tempDir.deleteOnExit()
-//
-//    try {
-//      // 2. Create configuration.tcfg
-//      val configFile = new File(tempDir, "configuration.tcfg")
-//      val configCode =
-//        """
-//          |building-includes{
-//          |    submap Floor1
-//          |    submap Floor2
-//          |    submap Floor3 using Floor2
-//          |    vehicle Elevator1
-//          |    vehicle OPS
-//          |}
-//          |""".stripMargin
-//      writeToFile(configFile, configCode)
-//
-//      // 3. Create map file (Floor1.tmap)
-//      val mapFile = new File(tempDir, "Floor1.tmap")
-//      val mapCode =
-//        """
-//          |topo-map Floor1() {
-//          |    let params: {permResident: Int} = {permResident = 2}
-//          |    topo-node node1
-//          |    topo-node node2
-//          |    atomic-path [node1 <-> node2] { cost = 10.0 }
-//          |}
-//          |""".stripMargin
-//      writeToFile(mapFile, mapCode)
-//
-//      val mapFile2 = new File(tempDir, "Floor2.tmap")
-//      val mapCode2 =
-//        """
-//          |topo-map Floor2() {
-//          |    let params: {permResident: Int} = {permResident = 10}
-//          |    topo-node node1
-//          |    topo-node node2
-//          |    atomic-path [node1 <-> node2] { cost = 7.0 }
-//          |}
-//          |""".stripMargin
-//      writeToFile(mapFile2, mapCode2)
-//
-//      // 4. Create transport file (Elevator1.ttr)
-//      val transFile = new File(tempDir, "Elevator1.ttr")
-//      val vehicleCode =
-//        """
-//          |transport Elevator1 is Elevator {
-//          |    let params: {velocity: Float, accl: Float, carAmount: Int, duty: Int} = {velocity = 2.5, accl = 0.8, carAmount = 4, duty = 1350}
-//          |    station s1 at Floor1::node1 { location = 0.0, departureRate = 0.9 }
-//          |    station s2 at Floor2::node1 { location = 5.0, departureRate = 0.1 }
-//          |    station s3 at Floor3::node1 { location = 10.0, departureRate = 0.0 }
-//          |}
-//          |""".stripMargin
-//      writeToFile(transFile, vehicleCode)
-//
-//      // 5. Run the compiler
-//      val compiler = new TopoScriptCompiler()
-//
-//      // We expect this to run without throwing exceptions
-//      val result = compiler.compile(tempDir.getAbsolutePath)
-//
-//      // Since the current implementation returns a placeholder, we just check it's not null
-//      // In the future, we would assert on result.graphs and result.transportGraph
-//      assert(result != null)
-//      println("Compilation result: ")
-//      pprintln(result)
-//      pprintln(result.graphs("Floor3").nodes)
-//      println("Compilation successful!")
-//
-//    } finally {
-//      // Cleanup happens automatically via deleteOnExit usually, but good to be explicit for recursive delete if needed
-//      // For simple files, standard deleteOnExit on dir might fail if not empty in Java < ?
-//      // But this is just a test. using IO
-//      tempDir.listFiles().foreach(_.delete())
-//      tempDir.delete()
-//    }
-//  }
-//
-//  private def writeToFile(file: File, content: String): Unit = {
-//    val writer = new PrintWriter(file)
-//    try {
-//      writer.write(content)
-//    } finally {
-//      writer.close()
-//    }
-//  }
+  test("Compiler should parse and elaborate example project") {
+    // 1. Create a temporary directory for the test project
+    val tempDir = Files.createTempDirectory("toponavi_test_project").toFile
+    tempDir.deleteOnExit()
+
+    try {
+      // 2. Create configuration.tcfg
+      val configFile = new File(tempDir, "configuration.tcfg")
+      val configCode =
+        """
+          |building-includes{
+          |    submap Floor1
+          |    submap Floor2
+          |    submap Floor3 using Zone1
+          |    submap Floor4 using Zone1
+          |    vehicle Elevator1
+          |    vehicle OPS
+          |}
+          |""".stripMargin
+      writeToFile(configFile, configCode)
+
+      // 3. Create map file (Floor1.tmap)
+      val mapFile = new File(tempDir, "Floor1.tmap")
+      val mapCode =
+        """
+          |topo-map Floor1() {
+          |    let params: {permResident: Int} = {permResident = 2}
+          |    topo-node node1
+          |    topo-node node2
+          |    atomic-path [node1 <-> node2] { cost = 10.0 }
+          |}
+          |""".stripMargin
+      writeToFile(mapFile, mapCode)
+
+      val mapFile2 = new File(tempDir, "Floor2.tmap")
+      val mapCode2 =
+        """
+          |topo-map Floor2() {
+          |    let params: {permResident: Int} = {permResident = 10}
+          |    topo-node node1
+          |    topo-node node2
+          |    atomic-path [node1 <-> node2] { cost = 7.0 }
+          |}
+          |""".stripMargin
+      writeToFile(mapFile2, mapCode2)
+
+      val mapFile3 = new File(tempDir, "Zone1.tmap")
+      val mapCode3 =
+        """
+          |topo-map Zone1() {
+          |    let params: {permResident: Int} = {permResident = 10}
+          |    topo-node hall
+          |    topo-node room
+          |    atomic-path [hall <-> room] { cost = 7.0 }
+          |}
+          |""".stripMargin
+      writeToFile(mapFile3, mapCode3)
+
+      // 4. Create transport file (Elevator1.ttr)
+      val transFile = new File(tempDir, "Elevator1.ttr")
+      val vehicleCode =
+        """
+          |transport Elevator1 is Elevator {
+          |    let params: {velocity: Float, accl: Float, carAmount: Int, duty: Int} = {velocity = 1.0, accl = 0.8, carAmount = 2, duty = 1350}
+          |    station s1 at Floor1::node1 { location = 0.0, departureRate = 0.7 }
+          |    station s2 at Floor2::node1 { location = 5.0, departureRate = 0.1 }
+          |    station s3 at Floor3::hall { location = 10.0, departureRate = 0.1 }
+          |    station s4 at Floor4::hall { location = 15.0, departureRate = 0.1 }
+          |}
+          |""".stripMargin
+      writeToFile(transFile, vehicleCode)
+
+      // 5. Run the compiler
+      val compiler = new TopoScriptCompiler()
+
+      // We expect this to run without throwing exceptions
+      val result = compiler.compile(tempDir.getAbsolutePath)
+
+      // Since the current implementation returns a placeholder, we just check it's not null
+      // In the future, we would assert on result.graphs and result.transportGraph
+      assert(result != null)
+      println("Compilation result: ")
+      pprintln(result)
+      pprintln(result.graphs("Floor3").nodes)
+      println("Compilation successful!")
+
+    } finally {
+      // Cleanup happens automatically via deleteOnExit usually, but good to be explicit for recursive delete if needed
+      // For simple files, standard deleteOnExit on dir might fail if not empty in Java < ?
+      // But this is just a test. using IO
+      tempDir.listFiles().foreach(_.delete())
+      tempDir.delete()
+    }
+  }
+
+  private def writeToFile(file: File, content: String): Unit = {
+    val writer = new PrintWriter(file)
+    try {
+      writer.write(content)
+    } finally {
+      writer.close()
+    }
+  }
 
   test("Compiler should parse and elaborate real project"){
     // Try to locate the examples directory relative to the module or project root
